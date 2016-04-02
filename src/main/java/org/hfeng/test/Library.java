790c0a123bbc6e5169ca28ec56a3f31983ce8f5c
Harri Feng <harrifeng@gmail.com>
2016-04-02 11:16:17 +0800

    first workable status, with gradle upgrade to 2.12

---
+++ src/main/java/org/hfeng/test/Library.java
@@ -0,0 +1,10 @@
+package org.hfeng.test;
+
+public class Library {
+    public boolean someLibraryMethod() {
+        return true;
+    }
+    public static void main(String[] args) {
+        System.out.println("Hello World From Libray");
+    }
+}
---
+++ src/test/java/org/hfeng/test/LibraryTest.java
@@ -0,0 +1,12 @@
+package org.hfeng.test;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class LibraryTest {
+    @Test public void testSomeLibraryMethod() {
+        Library classUnderTest = new Library();
+        assertTrue("someLibraryMethod should return 'true'", classUnderTest.someLibraryMethod());
+        System.out.println("in Library Test");
+    }
+}
