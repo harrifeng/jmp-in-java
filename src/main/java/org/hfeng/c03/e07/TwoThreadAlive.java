8b97e7b1395ca2dfce6ec235362bc46d4154d416
Harri Feng <harrifeng@gmail.com>
2016-04-02 18:32:42 +0800

    new examples

---
+++ src/main/java/org/hfeng/c03/e07/TwoThreadAlive.java
@@ -0,0 +1,29 @@
+package org.hfeng.c03.e07;
+
+public class TwoThreadAlive extends Thread {
+    @Override
+    public void run() {
+        for (int i = 0; i < 10; i++) {
+            printMsg();
+        }
+    }
+
+    public void printMsg() {
+        Thread t = Thread.currentThread();
+        String name = t.getName();
+        System.out.println("name=" + name);
+    }
+
+    public static void main(String[] args) {
+        TwoThreadAlive tt = new TwoThreadAlive();
+        tt.setName("my worker thread");
+
+        System.out.println("before start(), tt.isAlive()=" + tt.isAlive());
+        tt.start();
+        System.out.println("just after start(), tt.isAlive()=" + tt.isAlive());
+        for (int i = 0; i < 10; i++) {
+            tt.printMsg();
+        }
+        System.out.println("at the end of main, tt.isAlive()=" + tt.isAlive());
+    }
+}
